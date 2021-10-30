913afbe23cf21fad1e98bd4e280fe272a2015d75
German Gorelkin <german.gorelkin@gmail.com>
2021-10-30 13:06:16 +0300

    draft v0.2.0

---
+++ e2e/e2e_test.go
@@ -0,0 +1,250 @@
+package rabbitmq_session
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+
+	session "github.com/germangorelkin/rabbitmq-session"
+)
+
+var opts = &dockertest.RunOptions{
+	Repository:   "rabbitmq",
+	Tag:          "3.9",
+	ExposedPorts: []string{"5672"},
+	PortBindings: map[docker.Port][]docker.PortBinding{"5672": {{HostIP: "0.0.0.0", HostPort: "5672"}}},
+}
+
+func TestOpenClose(t *testing.T) {
+
+	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	log.Println("connect to docker")
+
+	// pulls an image, creates a container based on it and runs it
+	resource, err := pool.RunWithOptions(opts)
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+
+	defer pool.Purge(resource)
+	log.Printf("run container id=%s", resource.Container.ID)
+
+	// log.Println("container stoping")
+	// if err := pool.Client.StopContainer(resource.Container.ID, 0); err != nil {
+	// 	log.Printf("failed to stop container id=%s:%v", resource.Container.ID, err)
+	// }
+
+	logger, _ := zap.NewProduction()
+
+	session := session.New(session.SessionConfig{
+		ExchangeName: "exchange_test",
+		ExchangeType: "topic",
+		QueueName:    "queue_test",
+		BindingKey:   "test",
+		Addr:         "amqp://guest:guest@localhost:5672/",
+		Logger:       logger.Sugar().Named("rabbit"),
+	})
+	defer session.Close()
+
+	numOfMsg := 100
+
+	go func() {
+		for i := 0; i < numOfMsg; {
+			message := []byte(fmt.Sprintf("message %d", i))
+			if err := session.Push(message); err == nil {
+				i++
+			}
+		}
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(numOfMsg)
+	var receivedMsg uint64
+
+	if err := session.Subscribe(func(b []byte) error {
+		atomic.AddUint64(&receivedMsg, 1)
+		wg.Done()
+		return nil
+	}); err != nil {
+		t.Fatalf("failed to Subscribe")
+	}
+
+	if err := session.Subscribe(func(b []byte) error {
+		atomic.AddUint64(&receivedMsg, 1)
+		wg.Done()
+		return nil
+	}); err != nil {
+		t.Fatalf("failed to Subscribe")
+	}
+
+	// TODO wait with timeout
+	wg.Wait()
+
+	assert.Equal(t, numOfMsg, int(receivedMsg))
+}
+
+func TestOpenClose2(t *testing.T) {
+
+	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	log.Println("connect to docker")
+
+	// pulls an image, creates a container based on it and runs it
+	resource, err := pool.RunWithOptions(opts)
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+	defer pool.Purge(resource)
+	log.Printf("run container id=%s", resource.Container.ID)
+
+	logger, _ := zap.NewProduction()
+
+	session := session.New(session.SessionConfig{
+		ExchangeName: "exchange_test",
+		ExchangeType: "topic",
+		QueueName:    "queue_test",
+		BindingKey:   "test",
+		Addr:         "amqp://guest:guest@localhost:5672/",
+		Logger:       logger.Sugar().Named("rabbit"),
+	})
+	defer session.Close()
+
+	numOfMsg := 100
+
+	go func() {
+		for i := 0; i < numOfMsg; {
+			message := []byte(fmt.Sprintf("message %d", i))
+			if err := session.Push(message); err == nil {
+				i++
+
+				if i == numOfMsg/2 {
+					log.Println("container stoping")
+					if err := pool.Client.StopContainer(resource.Container.ID, 0); err != nil {
+						log.Printf("failed to stop container id=%s:%v", resource.Container.ID, err)
+					}
+
+					time.Sleep(1 * time.Second)
+
+					log.Println("container starting")
+					if err := pool.Client.StartContainer(resource.Container.ID, nil); err != nil {
+						log.Printf("failed to start container id=%s:%v", resource.Container.ID, err)
+					}
+				}
+			}
+		}
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(numOfMsg)
+	var receivedMsg uint64
+
+	if err := session.Subscribe(func(b []byte) error {
+		atomic.AddUint64(&receivedMsg, 1)
+		wg.Done()
+		return nil
+	}); err != nil {
+		t.Fatalf("failed to Subscribe")
+	}
+
+	if err := session.Subscribe(func(b []byte) error {
+		atomic.AddUint64(&receivedMsg, 1)
+		wg.Done()
+		return nil
+	}); err != nil {
+		t.Fatalf("failed to Subscribe")
+	}
+
+	wg.Wait()
+
+	assert.Equal(t, numOfMsg, int(receivedMsg))
+}
+
+func TestOpenClose3(t *testing.T) {
+
+	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
+	pool, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	log.Println("connect to docker")
+
+	// pulls an image, creates a container based on it and runs it
+	resource, err := pool.RunWithOptions(opts)
+	if err != nil {
+		log.Fatalf("Could not start resource: %s", err)
+	}
+	defer pool.Purge(resource)
+	log.Printf("run container id=%s", resource.Container.ID)
+
+	logger, _ := zap.NewProduction()
+
+	session := session.New(session.SessionConfig{
+		ExchangeName: "exchange_test",
+		ExchangeType: "topic",
+		QueueName:    "queue_test",
+		BindingKey:   "test",
+		Addr:         "amqp://guest:guest@localhost:5672/",
+		Logger:       logger.Sugar().Named("rabbit"),
+	})
+	defer session.Close()
+
+	numOfMsg := 100
+
+	go func() {
+		for i := 0; i < numOfMsg; {
+			message := []byte(fmt.Sprintf("message %d", i))
+			if err := session.Push(message); err == nil {
+				i++
+			}
+		}
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(numOfMsg)
+	var receivedMsg uint64
+
+	if err := session.Subscribe(func(b []byte) error {
+		atomic.AddUint64(&receivedMsg, 1)
+
+		if atomic.LoadUint64(&receivedMsg) == uint64(numOfMsg/2) {
+			log.Println("container stoping")
+			if err := pool.Client.StopContainer(resource.Container.ID, 0); err != nil {
+				log.Printf("failed to stop container id=%s:%v", resource.Container.ID, err)
+			}
+
+			time.Sleep(1 * time.Second)
+
+			log.Println("container starting")
+			if err := pool.Client.StartContainer(resource.Container.ID, nil); err != nil {
+				log.Printf("failed to start container id=%s:%v", resource.Container.ID, err)
+			}
+		}
+
+		wg.Done()
+		return nil
+	}); err != nil {
+		t.Fatalf("failed to Subscribe")
+	}
+
+	wg.Wait()
+
+	assert.Equal(t, numOfMsg, int(receivedMsg))
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,15 @@
 module github.com/germangorelkin/rabbitmq-session
 
-go 1.16
+go 1.17
 
 require (
 	github.com/streadway/amqp v1.0.0
 	go.uber.org/zap v1.19.1
+)
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
